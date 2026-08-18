fe5a7519e850c622065594198600c2b02c02b340
UEH <UEH@B507PC54>
2026-08-18 16:26:33 +0700

    Add project files.

---
+++ NguyenThanhHung/Session1/Support.cs
@@ -0,0 +1,11 @@
+﻿internal class Support
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("heheboy");
+        Console.WriteLine(67+36);
+        Console.WriteLine("AnhTruongNoiThat");
+        Console.WriteLine("DungDongVaiAnh");
+    }
+}
---
+++ NguyenThanhHung/Session1/Support2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NguyenThanhHung.Session1
+{
+    internal class Support2
+    {
+    }
+}
