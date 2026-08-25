642988529763600cfb69c0caa1808b2d13fa2386
Nguyen Thanh Hung <nguyentanhung.2000tgdd@gmail.com>
2026-08-25 15:14:29 +0700

    Baitapbuoi2

---
+++ NguyenThanhHung/BaiTapBuoi2/Class1.cs
@@ -0,0 +1,14 @@
+﻿internal class Support
+{
+    private static void Main(string[] args)
+    {
+        int celsius;
+        Console.Write("Nhap nhiet do: ");
+        bool ketqua = int.TryParse (Console.ReadLine(), out celsius);
+        int kelvin = celsius + 273;
+        int fahrenheit = (celsius *18/10)+32
+            Console.WriteLine(kelvin);
+            Console.WriteLine(fahrenheit);
+    }
+
+}
