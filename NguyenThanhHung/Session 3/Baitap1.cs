ec7a5b7fa177bd7185399ec8cbcd33879c732852
Nguyen Thanh Hung <nguyentanhung.2000tgdd@gmail.com>
2026-08-25 16:01:58 +0700

    Baitap1

--- NguyenThanhHung/Session 3/Baitap1.cs
+++ NguyenThanhHung/Session 3/Baitap1.cs
@@ -1,11 +1,17 @@
-﻿internal class EX01
+﻿internal class Ex01
 {
-	static void ex01()
+    /// <summary>
+    /// kelvin = celsius + 273 
+    /// fahrenheit = celsius x 18 / 10 + 32
+    /// </summary>
+    static void ex01()
 	{
 		Console.Write("Nhap vao do Celsius: ");
 		float celsius = float.Parse(Console.ReadLine());
+
 		float kelvin = celsius + 273;
 		float fahrenheit = celsius + 18 / 10 + 32;
+
         Console.WriteLine($"{celsius} C {kelvin} k {fahrenheit} F");
 	}
 	public static void Main( string[] args )
