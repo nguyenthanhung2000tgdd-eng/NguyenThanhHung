637cc8f23efebf80c139e0f4b7dc985a7d656638
Nguyen Thanh Hung <nguyentanhung.2000tgdd@gmail.com>
2026-08-25 16:21:06 +0700

    baitap

--- NguyenThanhHung/Session 3/Baitap1.cs
+++ NguyenThanhHung/Session 3/Baitap1.cs
@@ -13,9 +13,23 @@
 		float fahrenheit = celsius + 18 / 10 + 32;
 
         Console.WriteLine($"{celsius} C {kelvin} k {fahrenheit} F");
+    }
+    /// <summary>
+    /// surface= 4 * pi * radius squared
+    /// volume= 4 / 3 * pi * radius cubed
+    /// </summary>
+    static void ex02()
+	{
+        Console.Write("Nhap vao ban kinh hinh tru: ");
+        float radius = float.Parse(Console.ReadLine());
+
+		double surface = 4 * Math.PI * Math.Pow(radius, 2);
+		double volume = 4 / 3 * Math.PI * Math.Pow(radius, 3);
+        Console.WriteLine($"Shpere radius = {radius} surface = {surface} volume ={volume}");
 	}
 	public static void Main( string[] args )
 	{
 		 ex01();
+		 ex02();
 	}
 }
